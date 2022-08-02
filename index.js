20add74a6076ef60dc2dcc54788ee99fd48fb195
LoopFruits <Kensworldc@gmail.com>
2022-08-02 09:23:17 -0400

    commit tree 000a25457f13307bd13f205cf13e9a124701b984

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverse(word) {
+  //create an array from the input string a
+  const wordArray = word.split("");
+  //reverse the array
+  const reversedWordArray = wordArray.reverse();
+  //create a string from the reversed array
+  const reversedWord = reversedWordArray.join(""); 
+  //return the reversed string 
+  return reversedWord
 }
 
+
+
+function isPalindrome(word) {
+  // Write your algorithm here
+  const reversedWord = reverse(word);
+  //return word if word is palindrome(reversedWord)
+  return word === reversedWord
+}
+//return true if the string is a palindrome
+//return false if the string is not (if the word is the same as the word in reverse it should return true)
 /* 
   Add your pseudocode here
 */
-
+  //reverse the input string 
+  //if the reversed string is the same as the input 
+    //return true 
+    //else 
+    //return false 
 /*
   Add written explanation of your solution here
 */
